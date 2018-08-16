e6571d99af457d7e8f98675db95da1659cc9f62f
Rigoberto Saenz Imbacuan <beto456789@gmail.com>
2018-08-16 00:56:32 -0500

    First version of the project

---
+++ FinalProject/FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Rigoberto Sáenz Imbacuán on 8/16/18.
+//  Copyright © 2018 Rigoberto Saenz Imbacuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
