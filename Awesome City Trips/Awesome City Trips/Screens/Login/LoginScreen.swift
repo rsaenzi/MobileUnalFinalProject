c0f33127b99bff8bf302a59db6f6607742d8066d
Rigoberto Saenz Imbacuan <beto456789@gmail.com>
2018-11-19 00:56:54 -0500

    Login and SignUp wireframes added

---
+++ Awesome City Trips/Awesome City Trips/Screens/Login/LoginScreen.swift
@@ -0,0 +1,13 @@
+//
+//  LoginScreen.swift
+//  Awesome City Trips
+//
+//  Created by Rigoberto Sáenz Imbacuán on 11/18/18.
+//  Copyright © 2018 Awesome City Team. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+}
---
+++ Awesome City Trips/Awesome City Trips/Screens/SignUp/SignUpScreen.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpScreen.swift
+//  Awesome City Trips
+//
+//  Created by Rigoberto Sáenz Imbacuán on 11/18/18.
+//  Copyright © 2018 Awesome City Team. All rights reserved.
+//
+
+import UIKit
+
+class SignUpScreen: UIViewController {
+    
+    @IBAction func onTapCancel(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
